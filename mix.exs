8c87fdd4f4c665bc62655291bdbfe7a1100cc99f
lubien <lubien1996@gmail.com>
2023-01-08 20:16:03 -0300

    Install pubsub

--- mix.exs
+++ mix.exs
@@ -40,6 +40,7 @@ defmodule SessionPlayground.MixProject do
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.18.3"},
+      {:phoenix_pubsub, "~> 2.0"},
       {:heroicons, "~> 0.5"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.7.2"},
