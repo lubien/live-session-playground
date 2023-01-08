887c27b5438799e827601a2fa45ca31e987ae7c6
lubien <lubien1996@gmail.com>
2023-01-08 20:16:36 -0300

    Simple notifications

--- lib/session_playground_web/components/core_components.ex
+++ lib/session_playground_web/components/core_components.ex
@@ -123,6 +123,49 @@ defmodule SessionPlaygroundWeb.CoreComponents do
     """
   end
 
+  def notifications(assigns) do
+    ~H"""
+    <div
+      aria-live="assertive"
+      class="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
+    >
+      <div
+        id="notifications"
+        phx-update="prepend"
+        class="flex w-full flex-col items-center space-y-4 sm:items-end"
+      >
+        <div
+          :for={notification <- @notifications}
+          id={"notification-#{notification.id}"}
+          class="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5"
+        >
+          <div class="p-4">
+            <div class="flex items-start">
+              <div class="flex-shrink-0">
+                <Heroicons.check_circle outline class="h-6 w-6 text-green-400" />
+              </div>
+              <div class="ml-3 w-0 flex-1 pt-0.5">
+                <p class="text-sm font-medium text-gray-900"><%= notification.title %></p>
+                <p class="mt-1 text-sm text-gray-500"><%= notification.description %></p>
+              </div>
+              <div class="ml-4 flex flex-shrink-0">
+                <button
+                  phx-click={hide("#notification-#{notification.id}")}
+                  type="button"
+                  class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  <span class="sr-only">Close</span>
+                  <Heroicons.x_mark mini class="h-5 w-5" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    """
+  end
+
   @doc """
   Renders flash notices.
 
--- lib/session_playground_web/live/page_live.ex
+++ lib/session_playground_web/live/page_live.ex
@@ -1,3 +1,38 @@
 defmodule SessionPlaygroundWeb.PageLive do
   use SessionPlaygroundWeb, :live_view_without_layout
+
+  alias Phoenix.PubSub
+
+  def mount(_params, _session, socket) do
+    PubSub.subscribe(SessionPlayground.PubSub, "notifications")
+
+    {:ok,
+     socket
+     |> assign(:notifications, [random_notification(), random_notification()]),
+     temporary_assigns: [notifications: []]}
+  end
+
+  def handle_event("send_test_notification", _params, socket) do
+    PubSub.broadcast!(
+      SessionPlayground.PubSub,
+      "notifications",
+      {:notification, random_notification()}
+    )
+
+    {:noreply, socket}
+  end
+
+  def handle_info({:notification, notification}, socket) do
+    {:noreply, assign(socket, :notifications, [notification | socket.assigns.notifications])}
+  end
+
+  defp random_notification do
+    id = Ecto.UUID.generate()
+
+    %{
+      id: id,
+      title: "Test notification",
+      description: "ID #{id}"
+    }
+  end
 end
